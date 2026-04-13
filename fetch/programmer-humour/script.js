901b1e6af13dd563bacc2d8e69502a98c28f72f4
Elisabeth-Matulian <elisabethmatulian@gmail.com>
2026-04-13 19:35:21 +0100

    Incorporate error handling

--- fetch/programmer-humour/script.js
+++ fetch/programmer-humour/script.js
@@ -3,4 +3,7 @@ const image = document.getElementById("image");
 
 fetch(url)
     .then((response) => response.json())
-    .then((data) => image.src = data.img);
+    .then((data) => image.src = data.img)
+    .catch((error) => { 
+        throw(error)
+    });
